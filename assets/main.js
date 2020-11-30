91019354902c91c48aab9987aff2af82ff6aeec0
gurbuzertunga <gurbuzertunga@gmail.com>
2020-11-30 16:56:03 +0300

    add checkbox for read status

--- assets/main.js
+++ assets/main.js
@@ -1,26 +1,21 @@
-let myLibrary = [];
-
-let id = 0;
-let bookWrapper = document.getElementById("books");
-let titleValue = document.getElementById("title");
-let authorValue = document.getElementById("author");
-let pagesValue = document.getElementById("pages");
-let dropdown = document.getElementById("read");
-let dropdownValue = dropdown.options[dropdown.selectedIndex];
-let buttonValue = document.getElementById("submit");
-let bookList = document.getElementById('book-list');
-
+const bookWrapper = document.getElementById("books");
+const titleValue = document.getElementById("title");
+const authorValue = document.getElementById("author");
+const pagesValue = document.getElementById("pages");
+const dropdown = document.getElementById("read");
+const buttonValue = document.getElementById("submit");
+const bookList = document.getElementById('book-list');
 const formValue = document.getElementById("form");
 const getForm = document.getElementById("form-area");
-getForm.style.display = "none";
-
 const newBookBtn = document.getElementById("new-book");
-newBookBtn.addEventListener("click", () => (getForm.style.display = "block"));
-
-// const deleteButton = document.getElementById("delete");
-// deleteButton.addEventListener("click", removeBook);
-
 const closeForm = document.getElementById("close");
+
+let dropdownValue = dropdown.options[dropdown.selectedIndex];
+let myLibrary = [];
+let id = 0;
+
+getForm.style.display = "none";
+newBookBtn.addEventListener("click", () => (getForm.style.display = "block"));
 closeForm.addEventListener("click", () => (getForm.style.display = "none"));
 
 formValue.addEventListener("submit", stopRefresh);
@@ -76,19 +71,19 @@ function displayBook(newBook) {
     <td>${newBook.pages}</td>
     <td>${newBook.read}</td>
     <td><a href="#" id="delete" class="delete">Delete Book</a></td>
-  ` 
-  
-  // const deleteBookBtn = document.createElement('button');
-  // deleteBookBtn.classList("text-blue-500");
-  // bookWrapper.appendChild(deleteBookBtn);
-  // bookWrapper.insertAdjacentHTML('afterbegin',`<li><button type="reset" id="delete">delete the book</button><li>`);
-  // bookWrapper.insertAdjacentHTML('afterbegin',`<li class="text-blue-500" id="${id}">${newBook.read}</li>`);
-  // bookWrapper.insertAdjacentHTML('afterbegin',`<li class="text-blue-500" id="${id}">${newBook.pages}</li>`);
-  // bookWrapper.insertAdjacentHTML('afterbegin',`<li class="text-blue-500" id="${id}">${newBook.author}</li>`);
-  // bookWrapper.insertAdjacentHTML('afterbegin',`<li class="text-blue-500" id="${id}">${newBook.title}</li>`);
-  // // removeBook(id);
-  // let deleteBtn = document.getElementById('delete');
-  // deleteBtn.addEventListener('click', alert('I have clicked you!!!!'));
+    <td><input type="checkbox" name="reading-check" id="reading-check"></td>
+    `
+    let a = document.getElementById('reading-check');
+    if (dropdownValue.textContent === "Read") {
+      a.setAttribute('checked',true);
+    }
+     else  {
+      a;
+    }
+
+    readingButton = a.addEventListener('change', e => {
+      
+    })
 }
 
 bookList.addEventListener('click', function (e) {
