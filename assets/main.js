cd66b93d43233f715591c575429979a0997ad3d2
vanheaven <vanheaven6@gmail.com>
2020-12-08 23:45:47 +0300

    Update main.js
    Add generalFuctions module
    
    Add myMethods class

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,22 @@
-const titleValue = document.getElementById('title');
-const authorValue = document.getElementById('author');
-const pagesValue = document.getElementById('pages');
-const dropdown = document.getElementById('read');
-const buttonValue = document.getElementById('submit');
-const bookList = document.getElementById('book-list');
-const formValue = document.getElementById('form');
-const getForm = document.getElementById('form-area');
-const newBookBtn = document.getElementById('new-book');
-const closeForm = document.getElementById('close');
-const bookWrapper = document.getElementById('books');
+const titleValue = document.getElementById("title");
+const authorValue = document.getElementById("author");
+const pagesValue = document.getElementById("pages");
+const dropdown = document.getElementById("read");
+const buttonValue = document.getElementById("submit");
+const bookList = document.getElementById("book-list");
+const formValue = document.getElementById("form");
+const getForm = document.getElementById("form-area");
+const newBookBtn = document.getElementById("new-book");
+const closeForm = document.getElementById("close");
+const bookWrapper = document.getElementById("books");
 let books;
 
 const Store = (() => {
   const getBooksFromStore = () => {
-    if (localStorage.getItem('books') === null) {
+    if (localStorage.getItem("books") === null) {
       books = [];
     } else {
-      books = JSON.parse(localStorage.getItem('books'));
+      books = JSON.parse(localStorage.getItem("books"));
     }
     return books;
   };
@@ -24,170 +24,166 @@ const Store = (() => {
   const addBookToStore = (book) => {
     books = Store.getBooksFromStore();
     books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
+    localStorage.setItem("books", JSON.stringify(books));
   };
 
   const removeBookFromStore = (title) => {
     books = Store.getBooksFromStore();
-    books.forEach((book) => {
-      if (book.title === title) {
+    books.forEach((book, index) => {
+      if (book.getTitle === title) {
         books.splice(books.indexOf(book), 1);
-        localStorage.setItem('books', JSON.stringify(books));
+        localStorage.setItem("books", JSON.stringify(books));
       }
     });
   };
 
   const updateStoreElement = (books) => {
-    localStorage.setItem('books', JSON.stringify(books));
+    localStorage.setItem("books", JSON.stringify(books));
   };
 
   return {
-    addBookToStore, getBooksFromStore, removeBookFromStore, updateStoreElement,
+    addBookToStore,
+    getBooksFromStore,
+    removeBookFromStore,
+    updateStoreElement,
   };
 })();
 
 const myLibrary = Store.getBooksFromStore();
-// [
-//   {
-//     title: 'Lord of The Rings',
-//     author: 'J.R.R Tolkien',
-//     pages: 784,
-//     read: 'Read',
-//   },
-//   {
-//     title: 'The Dark Tower',
-//     author: 'Stephen King',
-//     pages: 485,
-//     read: 'Not Read',
-//   },
-// ];
-
 
 let id = 0;
 let dropdownValue = dropdown.options[dropdown.selectedIndex];
 
-// function Book(title, author, pages, read) {
-//   this.title = title;
-//   this.author = author;
-//   this.pages = pages;
-//   this.read = read;
-// }
-
-const Book1 = (title1, author1, pages1, read1) => {
-  const getTitle = title1;
-  const getAuthor = author1;
-  const getPages = pages1;
-  const getRead = read1;
+const Book = (title, author, pages, read) => {
+  const getTitle = title;
+  const getAuthor = author;
+  const getPages = pages;
+  const getRead = read;
   return {
-    getTitle, getAuthor, getPages, getRead,
+    getTitle,
+    getAuthor,
+    getPages,
+    getRead,
   };
 };
 
-function changeReadStatus(checkbox) {
-  const targetTd = checkbox.parentElement.previousElementSibling.previousElementSibling;
-  const refId = checkbox.id;
-  if (checkbox.checked) {
-    myLibrary[refId].read = 'Read';
-    books[refId].getRead = 'Read';
-    Store.updateStoreElement(books);
-    targetTd.textContent = 'Read';
-  } else {
-    myLibrary[refId].read = 'Not Read';
-    books[refId].getRead = 'Not Read';
-    Store.updateStoreElement(books);
-    targetTd.textContent = 'Not Read';
-  }
-}
+class myMethods {
+  static changeReadStatus = (checkbox) => {
+    const targetTd =
+      checkbox.parentElement.previousElementSibling.previousElementSibling;
+    const refId = checkbox.id;
+    if (checkbox.checked) {
+      books[refId].getRead = "Read";
+      Store.updateStoreElement(books);
+      targetTd.textContent = "Read";
+    } else {
+      books[refId].getRead = "Not Read";
+      Store.updateStoreElement(books);
+      targetTd.textContent = "Not Read";
+    }
+  };
 
+  static displayBook = (newBook) => {
+    id = books.indexOf(newBook);
+    const row = document.createElement("tr");
+    bookList.appendChild(row);
+    row.innerHTML = `
+      <td class="border border-black py-1">${newBook.getTitle}</td>
+      <td class="border border-black py-1">${newBook.getAuthor}</td>
+      <td class="border border-black py-1">${newBook.getPages}</td>
+      <td class="read border border-black py-1" id="${id}read">${newBook.getRead}</td>
+      <td class="border border-black py-1 text-center"><a href="#" id="delete" class="delete bg-red-800 px-2 text-white py-1 rounded">Delete Book</a></td>
+      <td class="border border-black py-1 text-center"><input class="my-auto" type="checkbox" name="checkbox" id='${id}'></td>
+      `;
 
-function displayBook(newBook) {
-  id = myLibrary.indexOf(newBook);
-  const row = document.createElement('tr');
-  bookList.appendChild(row);
-  row.innerHTML = `
-    <td class="border border-black py-1">${newBook.getTitle}</td>
-    <td class="border border-black py-1">${newBook.getAuthor}</td>
-    <td class="border border-black py-1">${newBook.getPages}</td>
-    <td class="read border border-black py-1" id="${id}read">${newBook.getRead}</td>
-    <td class="border border-black py-1 text-center"><a href="#" id="delete" class="delete bg-red-800 px-2 text-white py-1 rounded">Delete Book</a></td>
-    <td class="border border-black py-1 text-center"><input class="my-auto" type="checkbox" name="checkbox" id='${id}'></td>
-    `;
+    const checkBox = document.getElementById(id);
+    checkBox.addEventListener("click", (e) => myMethods.changeReadStatus(e.target));
 
-  const checkBox = document.getElementById(id);
-  checkBox.addEventListener('click', (e) => changeReadStatus(e.target));
-
-  if (myLibrary[id].read === 'Read') {
-    checkBox.setAttribute('checked', true);
-  }
+    if (books[id].getRead === "Read") {
+      checkBox.setAttribute("checked", true);
+    }
+  };
 }
 
 function addBook() {
-  if (titleValue.value === '' || authorValue.value === '' || pagesValue.value === '') {
-    const alertMessage = document.createElement('div');
+  if (
+    titleValue.value === "" ||
+    authorValue.value === "" ||
+    pagesValue.value === ""
+  ) {
+    const alertMessage = document.createElement("div");
     document.body.insertBefore(alertMessage, bookWrapper);
-    alertMessage.className = 'w-64 bg-red-800 text-white relative bottom-0 text-center text-lg mx-auto border-2 border-black rounded alert';
-    alertMessage.textContent = 'You need to fill the form!';
+    alertMessage.className =
+      "w-64 bg-red-800 text-white relative bottom-0 text-center text-lg mx-auto border-2 border-black rounded alert";
+    alertMessage.textContent = "You need to fill the form!";
     setTimeout(() => {
-      document.querySelector('.alert').remove();
+      document.querySelector(".alert").remove();
     }, 2000);
   } else {
-    const newBook = Book1(
+    const newBook = Book(
       titleValue.value,
       authorValue.value,
       pagesValue.value,
-      dropdownValue.textContent,
+      dropdownValue.textContent
     );
-    myLibrary.push(newBook);
     Store.addBookToStore(newBook);
-    displayBook(newBook);
+    myMethods.displayBook(newBook);
   }
 }
 
+const generalFunctions = (() => {
+  const deleteBook = (el) => {
+    if (el.classList.contains("delete")) {
+      const targetElement = el.parentElement.parentElement;
+      targetElement.remove();
+      const titleRef = el.parentElement.parentElement.childNodes[1].textContent;
+      Store.removeBookFromStore(titleRef);
+    }
+  };
 
-function deleteBook(el) {
-  if (el.classList.contains('delete')) {
-    const targetElement = el.parentElement.parentElement;
-    targetElement.remove();
-  }
-}
+  const displayFormOnClick = () => {
+    getForm.style.display = "block";
+    newBookBtn.style.display = "none";
+  };
 
-function displayFormOnClick() {
-  getForm.style.display = 'block';
-  newBookBtn.style.display = 'none';
-}
+  const hideFormOnClick = () => {
+    getForm.style.display = "none";
+    newBookBtn.style.display = "block";
+  };
 
-function hideFormOnClick() {
-  getForm.style.display = 'none';
-  newBookBtn.style.display = 'block';
-}
+  const stopRefresh = (e) => {
+    e.preventDefault();
+    titleValue.value = "";
+    authorValue.value = "";
+    pagesValue.value = "";
+  };
 
-function stopRefresh(e) {
-  e.preventDefault();
-  titleValue.value = '';
-  authorValue.value = '';
-  pagesValue.value = '';
-}
+  const selectChange = () => {
+    dropdownValue = dropdown.options[dropdown.selectedIndex];
+  };
 
-function selectChange() {
-  dropdownValue = dropdown.options[dropdown.selectedIndex];
-}
+  return {
+    selectChange,
+    stopRefresh,
+    hideFormOnClick,
+    displayFormOnClick,
+    deleteBook,
+  };
+})();
 
+getForm.style.display = "none";
+newBookBtn.addEventListener("click", generalFunctions.displayFormOnClick);
+closeForm.addEventListener("click", generalFunctions.hideFormOnClick);
 
-getForm.style.display = 'none';
-newBookBtn.addEventListener('click', displayFormOnClick);
-closeForm.addEventListener('click', hideFormOnClick);
+formValue.addEventListener("submit", generalFunctions.stopRefresh);
+buttonValue.addEventListener("click", addBook);
+buttonValue.addEventListener("click", generalFunctions.hideFormOnClick);
+dropdown.addEventListener("click", generalFunctions.selectChange);
 
-formValue.addEventListener('submit', stopRefresh);
-buttonValue.addEventListener('click', addBook);
-buttonValue.addEventListener('click', hideFormOnClick);
-dropdown.addEventListener('click', selectChange);
-
-bookList.addEventListener('click', (e) => {
-  deleteBook(e.target);
-  const titleRef = e.target.parentElement.parentElement.childNodes[1].textContent;
-  Store.removeBookFromStore(titleRef);
+bookList.addEventListener("click", (e) => {
+  generalFunctions.deleteBook(e.target);
 });
 
 myLibrary.forEach((book) => {
-  document.addEventListener('DOMContentLoaded', displayBook(book));
+  document.addEventListener("DOMContentLoaded", myMethods.displayBook(book));
 });
