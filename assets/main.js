64ab40fc4b77d3ac453f7f82fd76d7c683184b18
gurbuzertunga <gurbuzertunga@gmail.com>
2020-12-09 12:27:47 +0300

    added class refactor

--- assets/main.js
+++ assets/main.js
@@ -30,7 +30,7 @@ const Store = (() => {
   const removeBookFromStore = (title) => {
     books = Store.getBooksFromStore();
     books.forEach((book) => {
-      if (book.getTitle === title) {
+      if (book.title === title) {
         books.splice(books.indexOf(book), 1);
         localStorage.setItem('books', JSON.stringify(books));
       }
@@ -54,72 +54,84 @@ const myLibrary = Store.getBooksFromStore();
 let id = 0;
 let dropdownValue = dropdown.options[dropdown.selectedIndex];
 
-const Book = (title, author, pages, read) => {
-  const getTitle = title;
-  const getAuthor = author;
-  const getPages = pages;
-  const getRead = read;
-  return {
-    getTitle,
-    getAuthor,
-    getPages,
-    getRead,
-  };
-};
+// const Book = (title, author, pages, read) => {
+//   const getTitle = title;
+//   const getAuthor = author;
+//   const getPages = pages;
+//   const getRead = read;
+//   return {
+//     getTitle,
+//     getAuthor,
+//     getPages,
+//     getRead,
+//   };
+// };
 
-class myMethods {
+class Book {
+  constructor(title,author,pages,read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  
   static changeReadStatus = (checkbox) => {
     const targetTd = checkbox.parentElement.previousElementSibling.previousElementSibling;
+    console.log(targetTd);
     const refId = checkbox.id;
     if (checkbox.checked) {
-      books[refId].getRead = 'Read';
+      books[refId].read = 'Read';
       Store.updateStoreElement(books);
       targetTd.textContent = 'Read';
     } else {
-      books[refId].getRead = 'Not Read';
+      books[refId].read = 'Not Read';
       Store.updateStoreElement(books);
       targetTd.textContent = 'Not Read';
     }
   };
 
-  static displayBook = (newBook) => {
+  displayBook = (newBook) => {
     id = books.indexOf(newBook);
     const row = document.createElement('tr');
     bookList.appendChild(row);
+    console.log(newBook);
     row.innerHTML = `
-      <td class="border border-black py-1">${newBook.getTitle}</td>
-      <td class="border border-black py-1">${newBook.getAuthor}</td>
-      <td class="border border-black py-1">${newBook.getPages}</td>
-      <td class="read border border-black py-1" id="${id}read">${newBook.getRead}</td>
+      <td class="border border-black py-1">${newBook.title}</td>
+      <td class="border border-black py-1">${newBook.author}</td>
+      <td class="border border-black py-1">${newBook.pages}</td>
+      <td class="read border border-black py-1" id="${id}read">${newBook.read}</td>
       <td class="border border-black py-1 text-center"><a href="#" id="delete" class="delete bg-red-800 px-2 text-white py-1 rounded">Delete Book</a></td>
       <td class="border border-black py-1 text-center"><input class="my-auto" type="checkbox" name="checkbox" id='${id}'></td>
       `;
       
     const checkBox = document.getElementById(id);
-    checkBox.addEventListener('click', (e) => myMethods.changeReadStatus(e.target));
-    if (newBook.getRead === 'Read') {
+    checkBox.addEventListener('click', (e) => Book.changeReadStatus(e.target));
+    if (newBook.read === 'Read') {
       checkBox.checked = true;
     }
   };
-}
 
-function addBook() {
-  if (titleValue.value === '' || authorValue.value === '' || pagesValue.value === '') {
-    const alertMessage = document.createElement('div');
-    document.body.insertBefore(alertMessage, bookWrapper);
-    alertMessage.className =
-      'w-64 bg-red-800 text-white relative bottom-0 text-center text-lg mx-auto border-2 border-black rounded alert';
-    alertMessage.textContent = 'You need to fill the form!';
-    setTimeout(() => {
-      document.querySelector('.alert').remove();
-    }, 2000);
-  } else {
-    const newBook = Book(titleValue.value, authorValue.value, pagesValue.value, dropdownValue.value);
-    myLibrary.push(newBook);
-    Store.addBookToStore(newBook);
-    myMethods.displayBook(newBook);
+  addBook = () => {
+    if (titleValue.value === '' || authorValue.value === '' || pagesValue.value === '') {
+      const alertMessage = document.createElement('div');
+      document.body.insertBefore(alertMessage, bookWrapper);
+      alertMessage.className =
+        'w-64 bg-red-800 text-white relative bottom-0 text-center text-lg mx-auto border-2 border-black rounded alert';
+      alertMessage.textContent = 'You need to fill the form!';
+      setTimeout(() => {
+        document.querySelector('.alert').remove();
+      }, 2000);
+    } else {
+      newBook = new Book(titleValue.value, authorValue.value, pagesValue.value, dropdownValue.value);
+      myLibrary.push(newBook);
+      Store.addBookToStore(newBook);
+      newBook.displayBook(newBook);
+    }
   }
 }
+let newBook = new Book(titleValue.value, authorValue.value, pagesValue.value, dropdownValue.value);
+
+
 
 const generalFunctions = (() => {
   const deleteBook = (el) => {
@@ -127,6 +139,7 @@ const generalFunctions = (() => {
       const targetElement = el.parentElement.parentElement;
       targetElement.remove();
       const titleRef = el.parentElement.parentElement.childNodes[1].textContent;
+      console.log(titleRef);
       Store.removeBookFromStore(titleRef);
     }
   };
@@ -166,7 +179,7 @@ newBookBtn.addEventListener('click', generalFunctions.displayFormOnClick);
 closeForm.addEventListener('click', generalFunctions.hideFormOnClick);
 
 formValue.addEventListener('submit', generalFunctions.stopRefresh);
-buttonValue.addEventListener('click', addBook);
+buttonValue.addEventListener('click', newBook.addBook);
 buttonValue.addEventListener('click', generalFunctions.hideFormOnClick);
 dropdown.addEventListener('onChange', selectChange);
 
@@ -175,5 +188,5 @@ bookList.addEventListener('click', (e) => {
 });
 
 myLibrary.forEach((book) => {
-  document.addEventListener('DOMContentLoaded', myMethods.displayBook(book));
+  document.addEventListener('DOMContentLoaded', newBook.displayBook(book));
 });
